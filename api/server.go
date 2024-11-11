1acc14c686bccfef2137b115ab1d8b932fd36353
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-11-11 23:50:25 +0530

    snippet_service: add functionality to pick up user_id from the request and see if user is authenticated to create snippet fix derp!:server.go: wrap the snippets under the middleware authentication

--- api/server.go
+++ api/server.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
-	"github.com/vansh123456/pasterr/services"
 	"github.com/vansh123456/pasterr/middleware"
+	"github.com/vansh123456/pasterr/services"
 )
 
 // what we were doing in store.db,usko abstraction layer banake we used to connect with that,but we havent used that for simple CRUD functions so we are defining it here
@@ -20,26 +20,27 @@ func InitializeRouter(router *gin.Engine, dbConn *sql.DB) {
 	router.GET("/users", func(c *gin.Context) {
 		services.ListUsersHandler(c, dbConn)
 	})
-//url like localhost/snippets/createsnippets
-//localhost/snippets
-//localhost/snippets/:id
-	snippets := router.Group("/snippets",middleware.AuthMiddleware()){
+	//url like localhost/snippets/createsnippets
+	//localhost/snippets
+	//localhost/snippets/:id
+	snippets := router.Group("/snippets", middleware.AuthMiddleware())
+	{
 		router.POST("/createsnippets", func(c *gin.Context) {
 			services.CreateSnippetHandler(c, dbConn)
 		})
 		snippets.GET("", func(c *gin.Context) {
-            services.ListSnippetsHandler(c, dbConn)
-        })
-		outer.GET("/:id", func(c *gin.Context) {
+			services.ListSnippetsHandler(c, dbConn)
+		})
+		snippets.GET("/:id", func(c *gin.Context) {
 			services.GetSnippetByIDHandler(c, dbConn)
 		})
-		router.PUT("/:id", func(c *gin.Context) {
+		snippets.PUT("/:id", func(c *gin.Context) {
 			services.UpdateSnippetContent(c, dbConn)
 		})
-		router.DELETE("/:id", func(c *gin.Context) {
+		snippets.DELETE("/:id", func(c *gin.Context) {
 			services.DeleteSnippetHandler(c, dbConn)
 		})
 
 	}
-	
+
 }
---
+++ services/snippet_service.go
@@ -0,0 +1,87 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/vansh123456/pasterr/db/sqlc"
+)
+
+func CreateSnippetHandler(c *gin.Context, dbConn *sql.DB) {
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not authenticated"})
+		return
+	}
+
+	var params db.CreateSnippetParams
+	if err := c.BindJSON(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	params.UserID = int32(userID.(uint)) //UserID in the sqlc params is in int32 so we are converting it to int32 explicitely
+	fmt.Println(params)
+	queries := db.New(dbConn)
+	snippet, err := queries.CreateSnippet(c.Request.Context(), params)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error:failed to create snippet": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, snippet)
+}
+
+func ListSnippetsHandler(c *gin.Context, dbConn *sql.DB) {
+	queries := db.New(dbConn)
+	snippets, err := queries.ListSnippets(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve snippets"})
+		return
+	}
+
+	c.JSON(http.StatusOK, snippets)
+}
+
+func GetSnippetByIDHandler(c *gin.Context, dbConn *sql.DB) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error invalid ID": err.Error()})
+		return
+	}
+	queries := db.New(dbConn)
+	snippet, err := queries.GetSnippetByID(c.Request.Context(), int64(id))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error:failed to list snippets": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, snippet)
+}
+func UpdateSnippetContent(c *gin.Context, dbConn *sql.DB) {
+	var params db.UpdateSnippetContentParams
+	if err := c.BindJSON(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	queries := db.New(dbConn)
+	updatedsnippet, err := queries.UpdateSnippetContent(c.Request.Context(), params)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update snippet"})
+		return
+	}
+	c.JSON(http.StatusOK, updatedsnippet)
+}
+func DeleteSnippetHandler(c *gin.Context, dbConn *sql.DB) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error invalid ID to delete": err.Error()})
+		return
+	}
+	queries := db.New(dbConn)
+	if err := queries.DeleteSnippet(c.Request.Context(), int64(id)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete snippet"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Snippet deleted"})
+}
