8a850488e3e2bf488b3301917b91c08f8176840a
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-11-11 23:01:09 +0530

    bcrypt: add password hashing and hash pass checking

--- services/user_service.go
+++ services/user_service.go
@@ -2,11 +2,28 @@ package services
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/vansh123456/pasterr/db/sqlc"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("hashedpass:", string(hashedPassword))
+	return string(hashedPassword), nil
+}
+
+// check and compare the hashed Password
+func CheckPassword(plainPassword string, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil //default value is nil but if it is true it returns that
+}
 
 func SignupHandler(c *gin.Context, dbConn *sql.DB) {
 	var params db.CreateAccountParams
@@ -14,6 +31,12 @@ func SignupHandler(c *gin.Context, dbConn *sql.DB) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	hashedPassword, err := HashPassword(params.Password) //takes the json password and passed it to the functions above!
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+	params.Password = hashedPassword //updates the value going into the DB with the hashed Password
 	queries := db.New(dbConn)
 	user, err := queries.CreateAccount(c.Request.Context(), params)
 	if err != nil {
